511a37bcad299356a7f9601552a4aa6f2742158a
Leon Natanto <103975190+leonatanto@users.noreply.github.com>
2025-04-17 16:06:13 +0700

    Initial commit

---
+++ lib/theme.dart
@@ -0,0 +1,13 @@
+// import 'package:flutter/material.dart';
+// import 'package:google_fonts/google_fonts.dart';
+
+// Color mainColor = Color.fromARGB(255, 66, 151, 255);
+// Color secondaryColor = Color.fromARGB(255, 71, 117, 255);
+
+// Color blackColor = Colors.black;
+// Color whiteColor = Colors.white;
+
+// TextStyle mainTextStyle = GoogleFonts.poppins(
+//   fontWeight: FontWeight.w500,
+//   color: mainColor,
+// );
